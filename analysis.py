4e25247c5703e15d904ceb8d95bf38d3c7bd3b3f
zhurai <zhurai@users.noreply.github.com>
2019-02-01 11:39:56 -0800

    task 1a, added+tested the stop prices, while also putting some quick logic for the first 2 entries (exceptions)   (compare prices from current and previous 2 prices)

--- analysis.py
+++ analysis.py
@@ -28,10 +28,11 @@ with open(file) as csvfile:
 # Buy Signal = BuySellRatio > 1 && Sureness > 0.5 && Close > 5-EMA
 # Sell Signal = BuySellRatio < 1 && Sureness > 0.5 && Close < 5-EMA
 
-for row in table:
+for index,row in enumerate(table):
     # initialize
     date,priceopen,pricehigh,pricelow,priceclose,pricevolume,priceema,buysellratio,sureness = row
     priceopen=float(priceopen)
+    pricehigh=float(pricehigh)
     pricelow=float(pricelow)
     priceclose=float(priceclose)
     pricevolume=float(pricevolume)
@@ -46,12 +47,13 @@ for row in table:
     # debug
     #print(date + " ",end='')
 
+    #### SIGNALS
+
     # sureness
     if sureness > 0.5:
         issure=1
     else:
         issure=0
-
     # if buy signal
     if issure==1 and buysellratio > 1.0 and priceclose > priceema:
         isbuy=1
@@ -68,12 +70,39 @@ for row in table:
         row.append("NEUTRAL")
     #print("")
 
+    #### STOP PRICES
+    # min=longstop
+    # max=shortstop
+    # min= low of last 3 days including today
+    # max= high of last 3 days including today
+    low3day = []
+    high3day = []
+    if index==0:
+        # no other entries
+        low3day= [pricelow,pricelow,pricelow]
+        high3day= [pricehigh,pricehigh,pricehigh]
+    elif index==1:
+        # 1 other entry
+        low3day = [pricelow,float(table[index-1][3]),pricelow]
+        high3day = [pricehigh,float(table[index-1][2]),pricehigh]
+    else:
+        low3day = [pricelow,float(table[index-1][3]),float(table[index-2][3])]
+        high3day = [pricehigh,float(table[index-1][2]),float(table[index-2][2])]
+    longstop = min(low3day)
+    shortstop = max(high3day)
+    row.append(longstop)
+    row.append(shortstop)
+    
+
 
 # save file
 header.append("Signal")
+header.append("Long Stop")
+header.append("Short Stop")
 with open(ofile,mode='w',newline='') as csvfile:
     write=csv.writer(csvfile,delimiter=',')
     write.writerow(header)
     for row in table:
         write.writerow(row)
         
+
