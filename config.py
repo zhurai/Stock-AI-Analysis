f0b50d95e8290696c8c253b2c07123e69f60b39a
zhurai <zhurai@users.noreply.github.com>
2019-02-22 14:27:52 -0800

    simplifying config.py

--- config.py
+++ config.py
@@ -2,24 +2,6 @@ import configparser
 
 # Default Values
 configfile='config.ini'
-debug=False
-inputfile=''
-gdrivekey=''
-gdriverootdir=''
-gdrivecsvid=''
-days=0
 
 config = configparser.ConfigParser()
 config.read(configfile)
-
-debug=config['DEBUG'].getboolean('debug')
-
-inputfile=config['INPUT']['file']
-
-days=int(config['ANALYSIS']['days'])
-
-gdrivekey=config['GOOGLEAPI']['key']
-gdriverootdir=config['GOOGLEAPI']['dbroot']
-gdrivecsvid=config['GOOGLEAPI']['dbid']
-
-
